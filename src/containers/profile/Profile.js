473181973cb8ce36f6f35d50a52bc57478a00350
Abel-Blue <abelmgetnet@gmail.com>
2022-12-28 22:08:13 +0300

    modified

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -16,7 +16,7 @@ export default function Profile() {
   useEffect(() => {
     if (openSource.showGithubProfile === "true") {
       const getProfileData = () => {
-        fetch("../../../public/profile.json")
+        fetch("/public/profile.json")
           .then(result => {
             if (result.ok) {
               return result.json();
